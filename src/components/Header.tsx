e7111d052d0d516fcbc3f5675202da16639c3299
piperal <roland.piperal123@gmail.com>
2026-06-23 16:23:58 +0300

    added header and about me

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import './Header.css'
 
 function Header() {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <div id='headerContainer'>
+            <ul id='itemList'>
+                <li className="headerItem">Item1</li>
+                <li className="headerItem">Item2</li>
+                <li className="headerItem">Item3</li>
+            </ul>
+            <hr/>
+        </div>
+    )
 }
 
 export default Header
