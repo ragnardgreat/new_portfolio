eed53897905871a8f44f0c3d703935caa8c9111d
piperal <roland.piperal123@gmail.com>
2026-06-26 19:41:46 +0300

    added project styling

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,37 +5,55 @@ function Projects() {
         <div id='projects'>
             <h1 id='title'>Projects</h1>
             <div id="minikino" className="projectContainer">
-                <h2 className="projectTitle">Minikino</h2>
-                <label>Description:</label>
-                <h3>This is a description</h3>
-                <label className="linksLabel">Links: </label>
-                <ul>
-                    <li>Link1</li>
-                    <li>link2</li>
-                    <li>link3</li>
-                </ul>
+                <div className="imgContainer">
+                    <img src="./public/minikino.svg" className="projectTitle" width={300} /><br></br><br></br>
+                </div>
+                <div className="projectText">
+                    <h3>
+                        <p>Features</p>
+                        Image, video and audio file upload<br />
+                        Interactive game creation<br />
+                        Game step editing<br />
+                        Multiple categories
+                        <p>Making of</p>
+                        This project was built in a 5 member team in the span of 3 weeks,<br /> in an internship provided by Tallinn University
+                    </h3>
+                    <button className="linkBtn">GitHub</button>
+                </div>
             </div>
             <div id="scl" className="projectContainer">
-                <h2 className="projectTitle" >SCL.</h2>
-                <label>Description:</label>
-                <h3>This is a description</h3>
-                <label className="linksLabel">Links: </label>
-                <ul>
-                    <li>Link1</li>
-                    <li>link2</li>
-                    <li>link3</li>
-                </ul>
+                <div className="imgContainer">
+                    <img src="./public/scl.png" className="projectTitle" width={300} /><br></br><br></br>
+                </div>
+                <div className="projectText">
+                    <h3>
+                        <p>Features</p>
+                        Image, video and audio file upload<br />
+                        Interactive game creation<br />
+                        Game step editing<br />
+                        Multiple categories
+                        <p>Making of</p>
+                        This project was built in a 5 member team in the span of 3 weeks,<br /> in an internship provided by Tallinn University
+                    </h3>
+                    <button className="linkBtn">GitHub</button>
+                </div>
             </div>
             <div id="webstore" className="projectContainer">
-                <h2 className="projectTitle">Real Webstore</h2>
-                <label>Description:</label>
-                <h3>This is a description</h3>
-                <label className="linksLabel">Links: </label>
-                <ul>
-                    <li>Link1</li>
-                    <li>link2</li>
-                    <li>link3</li>
-                </ul>
+                <div className="imgContainer">
+                    <img src="./public/real.png" className="projectTitle" width={300} /><br></br><br></br>
+                </div>
+                <div className="projectText">
+                    <h3>
+                        <p>Features</p>
+                        Image, video and audio file upload<br />
+                        Interactive game creation<br />
+                        Game step editing<br />
+                        Multiple categories
+                        <p>Making of</p>
+                        This project was built in a 5 member team in the span of 3 weeks,<br /> in an internship provided by Tallinn University
+                    </h3>
+                    <button className="linkBtn">GitHub</button>
+                </div>
             </div>
         </div>
         <hr></hr>
