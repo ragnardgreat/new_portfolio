2da8eec988490aad6966093cc3db76dab3b1f694
piperal <roland.piperal123@gmail.com>
2026-06-30 13:23:06 +0300

    small change

--- src/components/NameCard.tsx
+++ src/components/NameCard.tsx
@@ -5,7 +5,7 @@ function NameCard() {
     return (
         <div id='containerTitle'>
             <h1 id="name">Roland Piperal</h1>
-            <h3 id="desc">Software Developer, 4+ years</h3><br />
+            <h3 id="desc">Software Developer, ~4 years</h3><br />
         </div>
     )
 }
